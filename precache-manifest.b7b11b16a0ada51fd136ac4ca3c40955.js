f86a3a484b71cd53052fe8d36582741a8958977c
amandabenevides <benevidesamanda13@gmail.com>
2020-10-16 13:00:11 -0300

    Updates

---
+++ precache-manifest.b7b11b16a0ada51fd136ac4ca3c40955.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7240f513065605aa4f66fd4e7cb502b8",
+    "url": "/desafioFDO/index.html"
+  },
+  {
+    "revision": "4eb7db39a20937020940",
+    "url": "/desafioFDO/static/css/main.04062433.chunk.css"
+  },
+  {
+    "revision": "c7d65189781124f4efc9",
+    "url": "/desafioFDO/static/js/2.51b44585.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/desafioFDO/static/js/2.51b44585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb7db39a20937020940",
+    "url": "/desafioFDO/static/js/main.541eba15.chunk.js"
+  },
+  {
+    "revision": "b6f533d3bdee4fb2ea2f",
+    "url": "/desafioFDO/static/js/runtime-main.4f4b8989.js"
+  }
+]);
